2a6add7cb6835cde25b95cd5822b610e9e74a0ad
Sam Lynch <samdawkins1998@gmail.com>
2021-12-01 13:29:21 -0500

    commit tree e78a3a75e73c5f8e115289dd704f1d93590cb4fc

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your code here
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error){
+//       alert("Bad things! Ragnarok!");
+//       console.log(error.messsage);
+//   })
+
+function sumbitData (name, email) {
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            name, email
+        })
+    })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (object){
+            document.body.innerHTML = object['id'];
+        })
+        .catch(function (error){
+            document.body.innerHTML = error.message
+        })
+    }
+sumbitData()
